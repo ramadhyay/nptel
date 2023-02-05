56e670fa25b0daeec656675e8d4ab7af33b66836
ramadhyay <ram.adhyay@gmail.com>
2023-02-05 22:29:16 +0530

    Sorting

---
+++ noc23_cs16/practice/week2/sort_insertion.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 19:26:22 2023
+
+@author: ram
+"""
+
+arr=[1,1,1,1,10,2,3,1,5,6,1,1,1]
+print(arr)
+
+def swap(arr, i,j):
+    arr[i],arr[j]=arr[j],arr[i]
+
+def insertion_sort_iter(arr):
+    for i in range(0, len(arr)):
+        for j in range(i,0,-1):
+            if arr[j] < arr[j-1]:
+                arr[j-1],arr[j]=arr[j],arr[j-1]
+
+
+# Insertion sort recursive
+# Insert value at 'pos' into array arr which is sotred 'before' poisition pos
+def insert(arr,pos):
+    for j in range(pos,0,-1):
+        if arr[j] < arr[j-1]:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+        
+
+def insertion_sort_recur(arr,pos):
+    if pos==len(arr):
+        return
+
+    insert(arr,pos)    
+    insertion_sort_recur(arr,pos+1)
+
+#insertion_sort_iter(arr)
+insertion_sort_recur(arr,0)
+
+print(arr)
---
+++ noc23_cs16/practice/week2/sort_merge.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 21:28:27 2023
+
+@author: ram
+"""
+
+arr=[1,3,5,10,2,2,0,1,2,3]
+
+# A more natural and intiutive way to look at array and also saves space while merge sort
+def merge(arr1,arr2):
+    arr=[]
+    
+    while (arr1 != [] or arr2 != []):
+        if arr1==[] and arr2 != []:
+            arr.append(arr2.pop(0))
+        
+        if arr2==[] and arr1 != []:
+            arr.append(arr1.pop(0))
+
+
+        if arr1 != [] and arr2 != []:
+            if arr1[0] <= arr2[0]:
+                arr.append(arr1.pop(0))
+            else:
+                arr.append(arr2.pop(0))    
+
+
+    return arr
+
+def merge2(arr1,arr2):
+    arr=[]
+    
+    i=0
+    j=0
+    
+    while ( i < len(arr1) or j < len(arr2) ):
+        print("1: i",i,"j",j)
+
+        if (j==len(arr2)) or (i < len(arr1) and arr1[i] <= arr2[j]):
+            arr.append(arr1[i])
+            i += 1
+        
+        print("2: i",i,"j",j)
+
+        if (i==len(arr1)) or (j < len(arr2) and arr1[i] > arr2[j]):
+            arr.append(arr2[j])
+            j += 1
+        
+        print("3: i",i,"j",j)
+        print("\n")
+        
+    return arr
+
+
+# Perform merge sort...!
+def merge_sort(arr):
+    
+    # Base condition t(1) = 1
+    if len(arr)==1:
+        return arr
+    
+    # Recurrence
+    mid=len(arr)//2
+    
+    leftarr=merge_sort(arr[0:mid])
+    rightarr=merge_sort(arr[mid:])
+    
+    return merge(leftarr,rightarr)
+
+
+print(arr)
+arr=merge_sort(arr)
+print(arr)
